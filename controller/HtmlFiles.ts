a5e6b5169acfa406a1b45c0df4c90f510a94afbf
kirengamartial <kirengamartial@gmail.com>
2024-04-19 17:34:07 +0200

    new changes

--- controller/HtmlFiles.ts
+++ controller/HtmlFiles.ts
@@ -4,31 +4,9 @@ import {Request, Response} from "express";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-export const indexFile = (req: Request, res: Response) => {
-    const indexPath = path.resolve(__dirname, '../../../Frontend/index.html');
-    res.sendFile(indexPath);
-}
-export const registerFile = (req: Request, res: Response) => {
-    const registerPath = path.resolve(__dirname, '../../../Frontend/register.html');
-    res.sendFile(registerPath);
-}
-export const editProfileFile = (req: Request, res: Response) => {
-    const registerPath = path.resolve(__dirname, '../../../Frontend/editProfile.html');
-    res.sendFile(registerPath);
-}
-
-export const contactFile = (req: Request, res: Response) => {
-    const contactPath = path.resolve(__dirname, '../../../Frontend/contact.html');
-    res.sendFile(contactPath);
-}
 export const adminQueryFile = (req: Request, res: Response) => {
     const adminQueryPath = path.resolve(__dirname, '../../../Frontend/adminquery.html');
     res.sendFile(adminQueryPath);
-}
-
-export const loginFile = (req: Request, res: Response) => {
-    const loginPath = path.resolve(__dirname, '../../../Frontend/login.html');
-    res.sendFile(loginPath);
 }
 
 export const adminArticleFile = (req: Request, res: Response) => {
@@ -45,25 +23,4 @@ export const adminEditArticleFile =  (req: Request, res: Response) => {
     const adminEditArticlePath = path.resolve(__dirname, '../../../Frontend/admineditarticle.html');
     res.sendFile(adminEditArticlePath);
 }
-
-export const blogFile =  (req: Request, res: Response) => {
-    const blogsPath = path.resolve(__dirname, '../../../Frontend/Blogs.html');
-    res.sendFile(blogsPath);
-}
-
-export const contentBlogFile = (req: Request, res: Response) => {
-    const contentBlogPath = path.resolve(__dirname, '../../../Frontend/ContentBlog1.html');
-    res.sendFile(contentBlogPath);
-}
-
-export const aboutFile = (req: Request, res: Response) => {
-    const aboutPath = path.resolve(__dirname, '../../../Frontend/about.html');
-    res.sendFile(aboutPath);
-}
-
-export const portfolioFile =  (req: Request, res: Response) => {
-    const portfolioPath = path.resolve(__dirname, '../../../Frontend/portfolio.html');
-    res.sendFile(portfolioPath);
-}
-
 
--- routes/HtmlFiles.ts
+++ routes/HtmlFiles.ts
@@ -1,34 +1,16 @@
 import { Router } from 'express'
 import {checkAuth} from '../middleware/authMiddleware.js'
-import {indexFile, registerFile, editProfileFile, contactFile, adminQueryFile, loginFile, adminArticleFile, adminAddArticleFile, adminEditArticleFile, blogFile, contentBlogFile, aboutFile, portfolioFile} from '../controller/HtmlFiles.js'
+import { adminQueryFile, adminArticleFile, adminAddArticleFile, adminEditArticleFile} from '../controller/HtmlFiles.js'
 
 const router = Router()
 
-router.get('/', indexFile);
-
-router.get('/register', registerFile);
-
-router.get('/profile/:id', editProfileFile);
-
-router.get('/contact', contactFile);
-
 router.get('/query', checkAuth, adminQueryFile);
-
-router.get('/logins', loginFile);
 
 router.get('/article', checkAuth, adminArticleFile);
 
 router.get('/add_article', checkAuth, adminAddArticleFile);
 
 router.get('/blogs/:id', checkAuth, adminEditArticleFile);
-
-router.get('/blogs', blogFile);
-
-router.get('/blogss/:id', contentBlogFile);
-
-router.get('/about', aboutFile);
-
-router.get('/portfolio', portfolioFile);
 
 
 
