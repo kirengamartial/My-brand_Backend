922e184b7603f02e6553f079252238ced1509467
kirengamartial <kirengamartial@gmail.com>
2024-04-24 18:05:29 +0200

    authorisation to admin

--- routes/Blog.ts
+++ routes/Blog.ts
@@ -1,4 +1,5 @@
 import {Router} from 'express'
+import { checkAuth } from '../middleware/authMiddleware.js'
 import {getAllBlog, getSingleBlog, createBlog, editBlog, deleteBlog} from '../controller/Blog.js'
 
 const router = Router()
@@ -6,8 +7,8 @@ const router = Router()
 
 router.get('/blog', getAllBlog)
 router.get('/api/blog/:id', getSingleBlog)
-router.post('/blog', createBlog);  
-router.put('/blog/:id', editBlog);
-router.delete('/blog/:id',deleteBlog);
+router.post('/blog',checkAuth, createBlog);  
+router.put('/blog/:id',checkAuth, editBlog);
+router.delete('/blog/:id',checkAuth, deleteBlog);
 
 export default router
